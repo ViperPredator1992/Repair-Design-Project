d657a729de217476b4305bf09103bd290865262e
AlexanderSemenov92 <42918575+AlexanderSemenov92@users.noreply.github.com>
2020-01-19 01:03:08 +0200

    feat: add header in index.html

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const cleanCSS = require('gulp-clean-css');
+const rename = require('gulp-rename');
+
+// Static server
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./src/"
+        }
+    });
+    gulp.watch("./src/*.html").on('change', browserSync.reload);
+});
+
+// CSS
+gulp.task('minify-css', function (done) {
+    gulp.src('./src/css/*.css')
+    .pipe(rename({suffix: '.min'}))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('dist/css/'))
+    done();
+});

48a3d518d0d233117885a9f07e74feddbd023b79
AlexanderSemenov92 <42918575+AlexanderSemenov92@users.noreply.github.com>
2020-01-19 14:33:28 +0200

    feat: add plugin gulp-sass in gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,38 @@
-const gulp = require('gulp');
+const {src, dest, watch} = require('gulp');
 const browserSync = require('browser-sync').create();
 const cleanCSS = require('gulp-clean-css');
 const rename = require('gulp-rename');
+const sass = require('gulp-sass');
 
 // Static server
-gulp.task('browser-sync', function() {
+function bs() {
+    serveSass();
     browserSync.init({
         server: {
             baseDir: "./src/"
         }
     });
-    gulp.watch("./src/*.html").on('change', browserSync.reload);
-});
+    watch("./src/*.html").on('change', browserSync.reload);
+    watch("./sass/**/*.sass").on('change', serveSass);
+    watch("./js/*.js").on('change', browserSync.reload);
+}
 
 // CSS
-gulp.task('minify-css', function (done) {
-    gulp.src('./src/css/*.css')
+function minifyCSS(done) {
+    src('./src/css/*.css')
     .pipe(rename({suffix: '.min'}))
     .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('dist/css/'))
-    done();
-});
+    .pipe(dest('dist/css/'));
+    //done();
+}
+minifyCSS();
+
+// SASS/SCSS
+function serveSass() {
+    return src("./sass/*.sass")
+        .pipe(sass())
+        .pipe(dest("./css"))
+        .pipe(browserSync.stream());
+}
+
+exports.serve = bs;
